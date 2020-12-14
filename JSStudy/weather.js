97abdadf76e84f790e0e384e3bf6c896026a2e48
DESKTOP-UGTRB8L\Ahn <licors214@gmail.com>
2020-12-14 20:30:29 +0900

    VanillaJS study - add weather api get data

--- JSStudy/weather.js
+++ JSStudy/weather.js
@@ -1,5 +1,21 @@
-const API_KEY = "wakdjalwjdl";
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "241051bf13976dd3ddadw41241231e";
 const COORDS = "coords";
+
+// 자바스크립트에서 요청을 보내고 정보를 받을 수 있다.
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5.weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -30,6 +46,8 @@ function loadCoords() {
         askForCoors();
     } else {
         // getWeather
+        const parseCoords = JSON.parse(loadedCords)
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
